cfc71f54262a1d0b36f5bde829a0edcc5a14e38a
vlad <jan@jvaas.com>
2020-09-12 00:08:26 +0200

    experimenting.

--- src/main/kotlin/io/jvaas/Scratch.kt
+++ src/main/kotlin/io/jvaas/Scratch.kt
@@ -15,6 +15,11 @@ class Visitor : PostgreSQLParserBaseVisitor<Unit>() {
 		super.visitSelect_stmt(ctx)
 	}
 
+	override fun visitStmt(ctx: PostgreSQLParser.StmtContext?) {
+		println(ctx?.text)
+		super.visitStmt(ctx)
+	}
+
 }
 
 object Scratch {
@@ -22,13 +27,39 @@ object Scratch {
 	@JvmStatic
 	fun main(args: Array<String>) {
 
-		val sql = """
-			SELECT * FROM table WHERE column = 1
-		""".trimIndent()
 
+		val sql =
+			// language=SQL
+			"""
+			INSERT INTO listing AS l			 (
+				-- core fields
+				l.section, l.id, l.created, l.modified, l.version,
+				-- basic fields
+				l.sale, l.rental, l.salePrice, l.rentalPrice, l.title, l.titleUrl, l.description,
+				-- detail fields
+				l.bedrooms, l.bathrooms, l.garages, l.parkings, l.floorSize, l.erfSize, l.ratesAndTaxes, l.levies
+				-- region fields
+				l.country, l.countryUrl, l.province, l.provinceUrl, l.city, l.cityUrl, l.suburb, l.suburbUrl
+				-- agent fields
+				-- admin fields
+			) VALUES (
+				-- core fields
+				?, ?, now(), now(), ?,
+				-- basic fields
+				?, ?, ?, ?, ?, ?, ?,
+				-- detail fields
+				?, ?, ?, ?, ?, ?, ?, ?,
+				-- region fields
+				?, ?, ?, ?, ?, ?, ?, ?
+			) ON CONFLICT (l.id) DO UPDATE SET
+				-- core fields
+				l.section = ?,
+				l.modified = now(),
+				l.version = listing.version + 1
+		""".trimIndent()
 		val lexer = PostgreSQLLexer(CharStreams.fromString(sql))
 		val parser = PostgreSQLParser(CommonTokenStream(lexer))
-		val parseTree: ParseTree = parser.???
+		val parseTree: ParseTree = parser.root()
 
 		Visitor().visit(parseTree)
 
