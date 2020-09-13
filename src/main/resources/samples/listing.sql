48188cc46b6172637222cda8170ad74f22148b47
vlad <jan@jvaas.com>
2020-09-14 00:37:23 +0200

    name queries.

--- src/main/resources/samples/listing.sql
+++ src/main/resources/samples/listing.sql
@@ -1,26 +1,46 @@
 
--- name=insertIdIntoListing
-INSERT INTO listing (id) VALUES (1);
+-- fun=insertListing
+INSERT INTO listing (id, created, modified) VALUES (?, now(), now());
+
+-- fun updateModified
+UPDATE listing SET modified = now() WHERE id = ?;
+-- fun updateTitle
+UPDATE listing SET modified = now(), title = ?, title_url = ? WHERE id = ?;
+-- fun updateDescription
+UPDATE listing SET modified = now(), description = ? WHERE id = ?;
+-- fun updateCounty
+UPDATE listing SET modified = now(), country = ?, country_url = ? WHERE id = ?;
+-- fun updateProvince
+UPDATE listing SET modified = now(), province = ?, province_url = ? WHERE id = ?;
+-- fun updateCity
+UPDATE listing SET modified = now(), city = ?, city_url = ? WHERE id = ?;
+-- fun updateSuburb
+UPDATE listing SET modified = now(), suburb = ?, suburb_url = ? WHERE id = ?;
+-- fun updateSale
+UPDATE listing SET modified = now(), sale = ? WHERE id = ?;
+-- fun updateRental
+UPDATE listing SET modified = now(), rental = ? WHERE id = ?;
+-- fun updateSalePrice
+UPDATE listing SET modified = now(), sale_price = ? WHERE id = ?;
+-- fun updateRentalPrice
+UPDATE listing SET modified = now(), rental_price = ? WHERE id = ?;
+-- fun updateBedroomsCount
+UPDATE listing SET modified = now(), bedrooms = ? WHERE id = ?;
+-- fun updateBathroomsCount
+UPDATE listing SET modified = now(), bathrooms = ? WHERE id = ?;
+-- fun updateGaragesCount
+UPDATE listing SET modified = now(), garages = ? WHERE id = ?;
+-- fun updateParkingsCount
+UPDATE listing SET modified = now(), parkings = ? WHERE id = ?;
+-- fun updateFloorSize
+UPDATE listing SET modified = now(), floor_size = ? WHERE id = ?;
+-- fun updateErfSize
+UPDATE listing SET modified = now(), erf_size = ? WHERE id = ?;
+-- fun updateRatesAndTaxes
+UPDATE listing SET modified = now(), rates_and_taxes = ? WHERE id = ?;
+-- fun updatePublished
+UPDATE listing SET modified = now(), published = ? WHERE id = ?;
+-- fun updateSection
+UPDATE listing SET modified = now(), section = ? WHERE id = ?;
 
 SELECT id FROM listing WHERE id = 1;
-
-UPDATE listing SET modified = now() WHERE id = 1;
-UPDATE listing SET title = '', title_url = '' WHERE id = 1;
-UPDATE listing SET description = '' WHERE id = 1;
-UPDATE listing SET country = '', country_url = '' WHERE id = 1;
-UPDATE listing SET province = '', province_url = '' WHERE id = 1;
-UPDATE listing SET city = '', city_url = '' WHERE id = 1;
-UPDATE listing SET suburb = '', suburb_url = '' WHERE id = 1;
-UPDATE listing SET sale = true WHERE id = 1;
-UPDATE listing SET rental = true WHERE id = 1;
-UPDATE listing SET sale_price = 1 WHERE id = 1;
-UPDATE listing SET rental_price = 1 WHERE id = 1;
-UPDATE listing SET bedrooms = 1 WHERE id = 1;
-UPDATE listing SET bathrooms = 1 WHERE id = 1;
-UPDATE listing SET garages = 1 WHERE id = 1;
-UPDATE listing SET parkings = 1 WHERE id = 1;
-UPDATE listing SET floor_size = 1 WHERE id = 1;
-UPDATE listing SET erf_size = 1 WHERE id = 1;
-UPDATE listing SET rates_and_taxes = 1 WHERE id = 1;
-UPDATE listing SET published = ? WHERE id = ?;
-UPDATE listing SET section = ? WHERE id = ?;
